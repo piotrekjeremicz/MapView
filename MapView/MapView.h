7bf57dc767ecae062fa7b08fbd502da05cccc227
Piotrek <piotr.jeremicz@ergo.digital>
2020-09-19 22:15:51 +0200

    Create MapView Xcode Framework project

---
+++ MapView/MapView.h
@@ -0,0 +1,19 @@
+//
+//  MapView.h
+//  MapView
+//
+//  Created by Piotrek on 19/09/2020.
+//  Copyright © 2020 Piotr Jeremicz. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MapView.
+FOUNDATION_EXPORT double MapViewVersionNumber;
+
+//! Project version string for MapView.
+FOUNDATION_EXPORT const unsigned char MapViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MapView/PublicHeader.h>
+
+
