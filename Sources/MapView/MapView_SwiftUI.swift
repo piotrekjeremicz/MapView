029207e558369817b028e1844ef19619f646574f
Piotrek <piotrek@jeremicz.com>
2021-06-23 20:20:34 +0200

    Refactor map configuration in SwiftUI UIViewRepresentable

--- Sources/MapView/MapView_SwiftUI.swift
+++ Sources/MapView/MapView_SwiftUI.swift
@@ -6,46 +6,82 @@
 //
 
 import SwiftUI
+import MapKit
 
 public struct Map: UIViewRepresentable {
- 
-    @usableFromInline internal let mapView: MapView
 
+    @Binding var region: MKCoordinateRegion
+        
     private var layers: [MapLayer] = []
+    private let configuration: Map.Configuration
     
-    public init(configuration: Configuration = Configuration(), @MapLayerBuilder _ layers: () -> [MapLayer]) {
+    internal var willChangeRegion: MapViewAction? = nil
+    internal var isChangingRegion: MapViewAction? = nil
+    internal var didChangeRegion: MapViewAction? = nil
+    
+    public init(
+        configuration: Configuration,
+        region: Binding<MKCoordinateRegion>,
+        @MapLayerBuilder _ layers: () -> [MapLayer],
+        willChangeRegion: MapViewAction? = nil,
+        isChangingRegion: MapViewAction? = nil,
+        didChangeRegion: MapViewAction? = nil
+    ) {
         self.layers = layers()
-        self.mapView = MapView()
+        self._region = region
+        self.configuration = configuration
+        
+        self.willChangeRegion = willChangeRegion
+        self.isChangingRegion = isChangingRegion
+        self.didChangeRegion = didChangeRegion
     }
     
     public func makeUIView(context: Context) -> MapView {
+        let mapView = MapView()
         mapView.updateConstraints()
         mapView.add(layers: layers)
+        
+        configure(mapView, with: configuration, context: context)
+        update(mapView)
         
         return mapView
     }
 
-    public func updateUIView(_ uiView: MapView, context: Context) {
-
+    public func updateUIView(_ mapView: MapView, context: Context) {
+        update(mapView)
     }
 }
 
 
 
 private extension Map {
-    func configureMap(_ configuration: Configuration) {
+    func configure(_ mapView: MapView, with configuration: Configuration, context: Context) {
+        mapView.region = region
         mapView.style = configuration.style
+        mapView.showsUserLocation = configuration.showsUserLocation
         
         mapView.isZoomEnabled = configuration.isZoomEnabled
         mapView.isPitchEnabled = configuration.isPitchEnabled
         mapView.isScrollEnabled = configuration.isScrollEnabled
         mapView.isRotateEnabled = configuration.isRotateEnabled
+        
+        mapView.willChangeRegion = willChangeRegion
+        mapView.isChangingRegion = isChangingRegion
+        mapView.didChangeRegion = didChangeRegion
+    }
+    
+    func update(_ mapView: MapView) {
+        mapView.remove(layers: mapView.layers)
+        mapView.add(layers: layers)
     }
 }
 
 public extension Map {
     struct Configuration {
         let style: MapStyle
+        let showsUserLocation: Bool
+        let userTrackingMode: Bool
+        
         let isZoomEnabled: Bool
         let isScrollEnabled: Bool
         let isPitchEnabled: Bool
@@ -53,34 +89,22 @@ public extension Map {
         
         public init(
             style: MapStyle = SystemMapStyle(),
+            showsUserLocation: Bool = false,
+            userTrackingMode: Bool = false,
+            
             isZoomEnabled: Bool = false,
             isScrollEnabled: Bool = false,
             isPitchEnabled: Bool = false,
             isRotateEnabled: Bool = false
         ) {
             self.style = style
+            self.showsUserLocation = showsUserLocation
+            self.userTrackingMode = userTrackingMode
+            
             self.isZoomEnabled = isZoomEnabled
             self.isPitchEnabled = isPitchEnabled
             self.isScrollEnabled = isScrollEnabled
             self.isRotateEnabled = isRotateEnabled
         }
-    }
-    
-    @inlinable func willChangeRegion(_ action: @escaping MapViewAction) -> Map {
-        self.mapView.willChangeRegion = action
-        
-        return self
-    }
-    
-    @inlinable func isChangingRegion(_ action: @escaping MapViewAction) -> Map {
-        self.mapView.isChangingRegion = action
-        
-        return self
-    }
-    
-    @inlinable func didChangeRegion(_ action: @escaping MapViewAction) -> Map {
-        self.mapView.didChangeRegion = action
-        
-        return self
     }
 }
