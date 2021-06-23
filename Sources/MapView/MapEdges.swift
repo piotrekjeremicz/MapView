ca86f43ae017b9f833c7643c2966f4355ca63d65
Piotrek <piotrek@jeremicz.com>
2021-06-23 19:13:28 +0200

    Added Map Edges

---
+++ Sources/MapView/MapEdges.swift
@@ -0,0 +1,18 @@
+//
+//  File.swift
+//  
+//
+//  Created by Piotrek on 24/05/2021.
+//
+
+import Foundation
+
+extension Map {
+    public struct Edges {
+        public let topLeft: Coordinate
+        public let topRight: Coordinate
+        
+        public let bottomLeft: Coordinate
+        public let bottomRight: Coordinate
+    }
+}
